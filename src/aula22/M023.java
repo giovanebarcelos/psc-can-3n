90da47f27bbe1a5e1e6ce8e4fb123c11069497b4
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-04-30 21:10:13 -0300

    doc: M023 - entedimento inicial

---
+++ src/aula22/M023.java
@@ -0,0 +1,27 @@
+package aula22;
+/*
+M023 A comissão organizadora de um rallye automobilístico decidiu apurar os resultados da competição
+através de um processamento eletrônico.
+Um dos algoritmos necessários para a classificação das equipes concorrentes é o que emite uma listagem
+geral do desempenho das equipes, atribuindo pontos segundo determinadas normas:
+O algoritmo deverá:
+a) Ler:
+a.1) uma linha contendo os tempos padrão (em minutos decimais) para as três fases de competição;
+a.2) um conjunto de linhas contendo cada uma o número de inscrição da equipe e os tempos (em minutos decimais)
+ que as mesmas despenderam ao cumprir as três diferentes etapas. A última linha (flag), que não entrará nos cálculos,
+  contém o número 9999 como número de inscrição.
+b) Calcular:
+b.1) os pontos de cada equipe em cada uma das etapas, seguindo o seguinte critério: Seja ∆ o valor absoluto
+da diferença entre o tempo-padrão (lido na primeira linha) e o tempo despendido pela equipe numa etapa:
+∆ < 3 minutos – atribuir 100 pontos à etapa
+3 <= ∆ <= 5 minutos – atribuir 80 pontos à etapa
+∆ > 5 minutos – atribuir 80 – (∆ - 5)/5 pontos à etapa
+b.2) o total de pontos de cada equipe nas três etapas;
+b.3) a equipe vencedora.
+c) Escrever:
+c.1) para cada equipe, o número de inscrição, os pontos obtidos em cada etapa e o total de
+pontos obtidos.
+ */
+
+public class M023 {
+}
