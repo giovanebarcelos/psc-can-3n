816e9a9593c4bc2d93a93ac0146565646609f5b5
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-05-28 20:52:56 -0300

    feat: soma dos n primeiros pares (R011)

---
+++ src/aula24/R011.java
@@ -0,0 +1,27 @@
+package aula24;
+
+/*
+R011 Escreva um programa que leia um valor inteiro n, calcule e mostre a soma dos n primeiros
+números pares. Exemplo: n = 3, apresentará 2 + 4 + 6 = 12.
+
+Authors:
+ */
+
+import java.util.Scanner;
+
+public class R011 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        int numero = read.nextInt();
+
+        int somaPares = 0;
+        
+        for (int i = 1, par = 2; i <= numero; i++, par += 2) {
+            System.out.print( par + (numero == i ? "" : " + "));
+            somaPares += par;
+        }
+        System.out.println(" = " + somaPares);
+    }
+}
